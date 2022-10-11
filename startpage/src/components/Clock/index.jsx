1bbf4ce1393031024b9e2effd9bd6c53f1d9326f
uncle-lv <uncle.lv@outlook.com>
2022-10-12 00:55:42 +0800

    ✨ feat: Clock

---
+++ startpage/src/components/Clock/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './style/index.css'
+
+
+const Clock = function(props) {
+
+    const getNowTime = () => {
+        const date = new Date();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        return `${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute}`;
+    };
+
+    const [ now, setNow ] = useState(getNowTime());
+
+    const tick = () => {
+        setNow(getNowTime());
+    };
+
+    useEffect(() => {
+        let ticker = setInterval(tick, 20000);
+        return function() {
+            clearInterval(ticker);
+        }
+    }, [])
+
+    return (
+        <div className='clock-wrapper'>
+            <div className='clock'>
+                <span>{now}</span>
+            </div>
+        </div>
+    )
+}
+
+export { Clock };
