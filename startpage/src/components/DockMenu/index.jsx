a7f04057f33e1c142a8130a9b8535696b6c02b56
uncle-lv <uncle.lv@outlook.com>
2022-10-13 16:36:30 +0800

    ✨ feat: tooltip and dropdown

--- startpage/src/components/DockMenu/index.jsx
+++ startpage/src/components/DockMenu/index.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/shift-away.css';
 import './style/index.css';
-
 import githubIcon from './style/icon/github.svg';
 import gitlabIcon from './style/icon/gitlab.svg';
 import gmailIcon from './style/icon/gmail.svg';
+import { useState } from 'react';
 
 
 const DockItem = function(props) {
     const { item } = props;
 
     return (
-        <div className='dock-item' onClick={() => { item.callback(item); }}>
-            <div className='item-icon' style={{
-                backgroundImage: `url(${item.icon})`
-            }}>
+        <Tippy content={item.name} placement='bottom' arrow={false} delay={[500, null]} animation='shift-away' theme='blur'>
+            <div className='dock-item' onClick={() => { item.callback(item); }}>
+                <div className='item-icon' style={{
+                    backgroundImage: `url(${item.icon})`
+                }}>
+                </div>
             </div>
-        </div>
+        </Tippy>
     )
 }
 
--- startpage/src/components/SearchBox/index.jsx
+++ startpage/src/components/SearchBox/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale-extreme.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle, faBilibili, faPython } from '@fortawesome/free-brands-svg-icons';
 import './style/index.css';
 
 
@@ -10,11 +12,13 @@ const EngineSelect = function(props) {
     const { options } = props;
 
     return (
-        <div>
-
+        <div className='engine-select-wrapper'>
+            <FontAwesomeIcon className='engine-icon' icon={faGoogle}></FontAwesomeIcon>
+            <FontAwesomeIcon className='engine-icon' icon={faBilibili}></FontAwesomeIcon>
+            <FontAwesomeIcon className='engine-icon' icon={faPython}></FontAwesomeIcon>
         </div>
     )
-}
+} 
 
 
 const SearchBox = function(props) {
@@ -23,9 +27,11 @@ const SearchBox = function(props) {
     return (
         <div className='search-box-wrapper'>
             <div className='search-box'>
-                <div className='prefix-icon' onClick={() => { console.log('click icon'); }}>
-                    <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
-                </div>
+                <Tippy content={<EngineSelect></EngineSelect>} allowHTML={true} trigger='click' placement='bottom' arrow={false} theme='blur' animation='scale-extreme'>
+                    <div className='prefix-icon' onClick={() => { console.log('click icon'); }}>
+                        <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
+                    </div>
+                </Tippy>
                 <input type='text'></input>
                 <div className='suffix-icon' onClick={() => { console.log('click search'); }}>
                     <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
